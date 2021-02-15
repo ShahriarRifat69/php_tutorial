7c5ddbedfb6c6207b8e72558126823bc80ee6a1d
ShahriarRifat69 <61321870+ShahriarRifat69@users.noreply.github.com>
2021-02-16 05:48:50 +0600

    added my fb profile link

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
          //echo using double quotes and interpolation
          echo "<h1>My Name is: $name</h1>";
     ?>
-    <button type="button" class="btn btn-dark">CLICK ME!</button>
+    <a href="https://www.facebook.com/aririkarishikikashi/" target="_blank" class="btn btn-dark">FB Profile</a>
     <button type="button" class="btn btn-primary">CLICK ME!</button>
     <button type="button" class="btn btn-success">CLICK ME!</button>
     <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku</a>
